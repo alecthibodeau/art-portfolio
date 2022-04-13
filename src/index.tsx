83166dd6ab92f3b21202b66fdf1a596c73df912e
alecthibodeau <alecthibodeau@gmail.com>
2022-04-12 21:20:05 -0400

    Replace ReactDOM.render with createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { StrictMode} from 'react';
+import { createRoot } from 'react-dom/client';
 
+import App from './App';
 import '../src/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
+// The correct ID of the root element is the ID of the div in index.html
+const rootElement = document.getElementById('root');
+
+// For TypeScript add the non-null (!) assertion operator
+const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
