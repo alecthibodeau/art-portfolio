491558929904ac8aee793abf38a48aff69356543
alecthibodeau <alecthibodeau@gmail.com>
2022-04-15 07:05:01 -0400

    Add prints images details

--- src/components/AllRoutes.tsx
+++ src/components/AllRoutes.tsx
@@ -15,7 +15,7 @@ function AllRoutes() {
       <Route path="/about" element={<About />} />
       <Route path="/drawings" element={<Gallery category="drawings" details={config.drawingsDetails} />} />
       <Route path="/posters" element={<Gallery category="posters" details={config.postersDetails} />} />
-      <Route path="/prints" element={<Gallery category="prints" details={config.drawingsDetails} />} />
+      <Route path="/prints" element={<Gallery category="prints" details={config.printsDetails} />} />
     </Routes>
   )
 };
