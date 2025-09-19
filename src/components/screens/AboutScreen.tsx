9d9cbc43b0e3f3c951aea38f9e19f13539400c94
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-09-19 15:13:29 -0400

    feat: rewrite about component's text to include additional information

--- src/components/screens/AboutScreen.tsx
+++ src/components/screens/AboutScreen.tsx
@@ -41,13 +41,15 @@ function AboutScreen(): JSX.Element {
           Alec Thibodeau is a visual artist. This website is a portfolio for his
           explorations in drawing, printmaking and related media. Visit
           Alec's <a href="https://huetown.com">huetown.com</a> site to learn about
-          additional work, including pieces currently available for sale.
-          Or find his coding projects at <a href="https://alect.me">alect.me</a>.
+          additional work, including pieces currently available for sale
+          and also <a href="https://huetown.com/moon-space"><span className="fst-italic">Moon
+          Space</span></a>, an interactive lunar calendar with animated clouds and
+          stars. Find more of Alec's coding projects at <a href="https://alect.me">alect.me</a>.
         </p>
         <p>
           Alec's <Link to={`/${config.epochTravelersRoute}`} onClick={scrollToTop}><span className="fst-italic">
           Epoch Travelers</span></Link> is a large-scale drawing incised into marble
-          that's permanently installed at an interior site in New York City. This
+          that's permanently installed at an interior site in New York City, NY. This
           piece was commissioned by the NYC Department of Education and the NYC School
           Construction Authority Public Art for Public Schools Program in collaboration with
           the <a href="https://www1.nyc.gov/site/dclapercentforart/projects/projects-detail.page?recordID=241" target="_blank" rel="noreferrer">NYC
@@ -57,10 +59,12 @@ function AboutScreen(): JSX.Element {
           </Link>, an experimental currency which has circulated internationally.
         </p>
         <p>
-          Galleries that have shown Alec's work include Tomato House (New York City),
-          Secret Project Robot (New York City), Spudnik Press Cooperative (Chicago),
-          Cade Tompkins Projects (Providence), World's Fair Gallery (Providence), the University of Tennessee's Ewing Gallery (Knoxville),
-          Boston Center for the Arts Mills Gallery and the University of Alabama at Birmingham's Visual Arts Gallery.
+          Galleries that have shown Alec's work include Tomato House (New York City, NY),
+          Secret Project Robot (New York City, NY), Spudnik Press Cooperative (Chicago, IL),
+          Cade Tompkins Projects (Providence, RI), World's Fair Gallery (Providence, RI),
+          the University of Tennessee's Ewing Gallery (Knoxville, TN), Boston Center
+          for the Arts Mills Gallery and the University of Alabama at Birmingham's
+          Visual Arts Gallery.
         </p>
       </div>
       <ContactForm
